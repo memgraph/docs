964fc52eaa7a1911ea7fb909da66c5104b946663
minime <marija.stupar@gmail.com>
2018-09-10 20:08:44 +0200

    v1.0.0 readme, folder structure jekyll frontmatter

--- html/html_links_filter.py
+++ html/html_links_filter.py
@@ -3,7 +3,7 @@ import panflute as pf
 
 def action(elem, doc):
     if isinstance(elem, pf.Link) and elem.url.endswith('.md'):
-        elem.url = elem.url[:-3] + '.html'
+        elem.url = "/" + elem.url[:-3]
         return elem
 
 
