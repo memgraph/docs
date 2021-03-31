44798ed87881fdc90c71fd53a22a738160f5b2ec
Ivan Despot <ivan.g.despot@outlook.com>
2021-03-31 09:56:09 +0200

    Add Facebook pixel

--- src/components/SegmentCall.js
+++ src/components/SegmentCall.js
@@ -1,12 +1,23 @@
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import { hotjar } from 'react-hotjar';
 
+const options = {
+    autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+    debug: false, // enable logs
+};
+
+
+
 export default (function () {
     if (!ExecutionEnvironment.canUseDOM) {
         return null;
     }
 
     return {
+        componentDidMount() {
+            const ReactPixel = require('react-facebook-pixel');
+            ReactPixel.default.init('880656202502108', options);
+        },
         onRouteUpdate({ location }) {
             window.analytics.page({
                 title: document.title,
--- static/js/load-analytics.js
+++ static/js/load-analytics.js
@@ -33,3 +33,4 @@
   r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
   a.appendChild(r);
 }(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+ 
