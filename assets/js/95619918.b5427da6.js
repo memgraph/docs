44bbbe735d37940ecff65fc53cfd039bbcace4e1
g-despot <g-despot@users.noreply.github.com>
2021-04-14 08:12:27 +0000

    deploy: 04b4b03a0778b774ef93c57c6a99a756ef6af238

---
+++ assets/js/95619918.b5427da6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[151],{225:function(e){e.exports=JSON.parse('{"pluginId":"cloud","version":"current","label":"Next","isLast":true,"docsSidebars":{"cloud":[{"type":"link","label":"Getting started","href":"/memgraph-cloud/"}]},"permalinkToSidebar":{"/memgraph-cloud/":"cloud"}}')}}]);
