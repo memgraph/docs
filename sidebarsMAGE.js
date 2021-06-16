e8b16adccfbcd5c69be96404c991de60ee0b56fa
jmatak <josip.matak@hotmail.com>
2021-06-16 11:43:14 +0200

    Add sidebar for betweenness centrality

--- sidebarsMAGE.js
+++ sidebarsMAGE.js
@@ -12,6 +12,7 @@ module.exports = {
       type: 'category',
       label: 'Query modules',
       items: [
+        'query-modules/python/betweenness-centrality',
         'query-modules/python/biconnected-components',
         'query-modules/python/bipartite-matching',
         'query-modules/python/bridges',
