c926e2220b6bfe4cb5f4e41e5c4225e30edc7984
Marko Francekovic <francekovic.marko@gmail.com>
2021-07-02 11:54:10 +0200

    Connect to memgraph page (#160)
    * connect to memgraph wip
    
    * connect to memgraph page
    
    * added all options to connect page
    
    * icon fixup
    
    * mgconsole icon fixup
    
    * Add url to ConnectButton
    
    * Remove mgconsole OS specific buttons

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -3,15 +3,19 @@ import styles from './container.module.css';
 
 export default function Container(props) {
 
-    const component = 'mg-container';
-
     const {
         size='default',
         children,
     } = props;
 
+    let sizeClass = null;
+
+    if (size === 'small') {
+        sizeClass = styles.mgContainer__small;
+    }
+
     return (
-        <div className={styles.mgContainer}>
+        <div className={`${styles.mgContainer} ${sizeClass}`}>
             {children}
         </div>
     );
--- src/theme/sections/home-callouts/home-callouts.js
+++ src/theme/sections/home-callouts/home-callouts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './home-callouts.module.css';
 import CalloutBox from "../../../components/callout-box/callout-box";
 import Container from "../../../components/container/container";
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function HomeCallouts() {
     const callouts = [
@@ -15,13 +16,13 @@ export default function HomeCallouts() {
             title: 'Run Memgraph in the cloud',
             subtitle: 'Explore your data in the cloud through our fully-managed service.',
             urlText: 'Get Started with Cloud',
-            url: 'https://memgraph.com/product/cloud',
+            url: 'https://memgraph.com/product/clouad',
         },
         {
             title: 'Connect to Memgraph',
             subtitle: 'Connect to Memgraph with your preferred programming language, via CLI console or the Memgraph Lab GUI.',
             urlText: 'Start exploring your data',
-            url: 'https://docs.memgraph.com/memgraph/connect-to-memgraph',
+            url: useBaseUrl('connect-to-memgraph'),
         },
         {
             title: 'Query, analyze & visualize the data',
