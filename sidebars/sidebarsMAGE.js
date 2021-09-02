f83e036120ee7a48233e1978baa4b367feff9f50
g-despot <66276597+g-despot@users.noreply.github.com>
2021-09-02 09:42:56 +0200

    Add uuid query module (#183)

--- sidebars/sidebarsMAGE.js
+++ sidebars/sidebarsMAGE.js
@@ -27,6 +27,7 @@ module.exports = {
         'query-modules/python/set-cover',
         'query-modules/python/tsp',
         'query-modules/python/union-find',
+        'query-modules/cpp/uuid',
         'query-modules/python/vrp',
         'query-modules/cpp/weakly-connected-components'],
     },
