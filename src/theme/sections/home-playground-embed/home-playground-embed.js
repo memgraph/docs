677ecc7843459d0a7ff120c102a52c6220f39abe
katarinasupe <supe.katarina@gmail.com>
2022-05-10 12:34:21 +0200

    Remove unnecessary code

--- src/theme/sections/home-playground-embed/home-playground-embed.js
+++ src/theme/sections/home-playground-embed/home-playground-embed.js
@@ -1,41 +1,8 @@
 import React from "react";
 import styles from "./home-playground-embed.module.css";
 import Container from "../../../components/container/container";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import ThemedImage from "@theme/ThemedImage";
 
 export default function HomePlaygroundEmbed() {
-    const items = [
-        {
-            title: "GitHub",
-            urlText: "Explore",
-            url: "https://github.com/memgraph",
-            img: useBaseUrl("img/github.svg"),
-            imgDark: useBaseUrl("img/github-dark-mode.svg"),
-        },
-        {
-            title: "Discord",
-            urlText: "Join",
-            url: "https://discord.gg/memgraph",
-            img: useBaseUrl("img/discord.svg"),
-            imgDark: useBaseUrl("img/discord-dark-mode.svg"),
-        },
-        {
-            title: "Stack Overflow",
-            urlText: "Ask Away",
-            url: "https://stackoverflow.com/questions/tagged/memgraphdb",
-            img: useBaseUrl("img/stack-overflow.svg"),
-            imgDark: useBaseUrl("img/stack-overflow-dark-mode.svg"),
-        },
-        {
-            title: "Developer Forum",
-            urlText: "Join",
-            url: "https://discourse.memgraph.com/",
-            img: useBaseUrl("img/dev-forum.svg"),
-            imgDark: useBaseUrl("img/dev-forum-dark-mode.svg"),
-        },
-    ];
-
     return (
         <div className={styles.homePlaygroundEmbed}>
             <Container>
