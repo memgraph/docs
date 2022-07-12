0bb90b1f9d8457016f9298723c9aa2e01473a30d
Kruno Golubic <kgolubic@gmail.com>
2022-07-12 12:12:08 +0200

    Update sidebar items

--- sidebars/sidebarsMemgraph.js
+++ sidebars/sidebarsMemgraph.js
@@ -316,7 +316,6 @@ module.exports = {
       link: { type: "doc", id: "under-the-hood/overview" },
       items: [
         "under-the-hood/storage",
-        //"under-the-hood/graph-algorithms",
         "under-the-hood/indexing",
         "under-the-hood/replication",
         "under-the-hood/telemetry",
