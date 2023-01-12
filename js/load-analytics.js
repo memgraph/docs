24b67d80969690e46c81c04970bcf4410daaf061
katarinasupe <katarinasupe@users.noreply.github.com>
2023-01-12 11:22:30 +0000

    deploy: 8c9815cfd3cba5b34fff2e71220f272c4293df20

--- js/load-analytics.js
+++ js/load-analytics.js
@@ -75,6 +75,28 @@ Function("J/xaw-lCiurrtu$yEn`msF#-9?='[s8QXd91={qGYld6`vR3Rb9uT:D!ZT[9od[hG$@O+`
   a.appendChild(r);
 })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
 
+!(function (f, b, e, v, n, t, s) {
+  if (f.fbq) return;
+  n = f.fbq = function () {
+    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  };
+  if (!f._fbq) f._fbq = n;
+  n.push = n;
+  n.loaded = !0;
+  n.version = "2.0";
+  n.queue = [];
+  t = b.createElement(e);
+  t.async = !0;
+  t.src = v;
+  s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s);
+})(
+  window,
+  document,
+  "script",
+  "https://connect.facebook.net/en_US/fbevents.js"
+);
+
 !(function (ss) {
   window.ldfdr = function () {
     (ldfdr._q = ldfdr._q || []).push([].slice.call(arguments).concat(ss));
